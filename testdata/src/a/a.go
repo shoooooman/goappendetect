f0df14285e398c3765b15190e9d833361e23b7a9
shoooooman <shoma216@gmail.com>
2020-09-04 17:22:31 +0900

    add a test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -6,30 +6,36 @@ func f1(s []int) {
 	s = append(s, 1) // want "this assignment is not detected outside of the func"
 }
 
-func f2(s *[]int) {
-	*s = append(*s, 1) // OK
+func f2(s []int) {
+	s, _ = append(s, 1), "foo" // want "this assignment is not detected outside of the func"
 }
 
 func f3(s []int) {
+	s = append(s, 1) // want "this assignment is not detected outside of the func"
+	s = append(s, 2) // want "this assignment is not detected outside of the func"
+}
+
+type A struct{}
+
+func (a A) f4(s []int) {
+	s = append(s, 1) // want "this assignment is not detected outside of the func"
+}
+
+func f5(s *[]int) {
+	*s = append(*s, 1) // OK
+}
+
+func f6(s []int) {
 	var t []int
 	t = append(s, 1) // OK
 	fmt.Println(t)
 }
 
-func f4(s []int) {
+func f7(s []int) {
 	t := make([]int, 0)
 	s = append(t, 1) // OK
 }
 
-func f5(s []int) {
-	s, _ = append(s, 1), "foo" // want "this assignment is not detected outside of the func"
-}
-
-func f6(s []int) {
+func f8(s []int) {
 	s, _ = make([]int, 0), append(s, 1) // OK
-}
-
-func f7(s []int) {
-	s = append(s, 1) // want "this assignment is not detected outside of the func"
-	s = append(s, 2) // want "this assignment is not detected outside of the func"
 }
